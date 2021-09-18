10e4ba7525f1c3b1765a3e7f7d63296ac523c2ae
Srishti Sharma <srishtishar@gmail.com>
2021-09-18 23:02:11 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'
 
-//console.log = function () {}
+console.log = function () {}
 
 ReactDOM.render(
   <App />,
